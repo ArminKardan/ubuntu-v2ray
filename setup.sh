ada5718370aae8f5f4966693e4dc6c1dc1ea706c
arminkardan <$armin.fire@gmail.com>
2024-07-04 15:39:27 +0330

    Update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 
 
 if [ -e "/usr/local/bin/v2ray" ]; then
-  
+  echo "V2ray is already installed, how to use: v2ray run -c <config.json>"
 else
   echo "V2Ray installing..."
   curl  -L -o "/usr/local/bin/v2ray" "https://raw.githubusercontent.com/ArminKardan/v2ray/main/v2ray"
+  echo "V2ray is already installed, how to use: v2ray run -c <config.json>"
 fi
-
-v2ray run 
 
 
