de08efafae5f2ec211f5b4274cfca9064c1093c3
arminkardan <$armin.fire@gmail.com>
2024-07-04 16:05:16 +0330

    Update

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ else
   echo "V2Ray installing..."
   curl  -L -o "/usr/local/bin/v2ray" "https://raw.githubusercontent.com/ArminKardan/ubuntu-v2ray/main/v2ray"
   curl  -L -o "/usr/local/bin/v2ctl" "https://raw.githubusercontent.com/ArminKardan/ubuntu-v2ray/main/v2ctl"
-  chmod 777 /usr/local/bin/v2ray
-  chmod 777 /usr/local/bin/v2ctl
+  sudo chmod 777 /usr/local/bin/v2ray
+  sudo chmod 777 /usr/local/bin/v2ctl
   echo "V2ray is already installed, how to use: v2ray run -c <config.json>"
 fi
 
